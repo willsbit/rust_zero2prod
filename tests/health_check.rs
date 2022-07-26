ba1d3516d153def2ca234ddfae3241caa3f2c3c8
Willian <willsbit@gmail.com>
2022-07-26 14:30:06 -0300

    [TEST] Adjust integration test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,12 @@
+use std::net::TcpListener;
+
 #[actix_rt::test]
 async fn health_check_works() {
-    spawn_app();
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request");
@@ -13,7 +15,17 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    // Port 0 is special-cased at the OS level:
+    // trying to bind port 0 will trigger an OS scan for an available port
+    // which will then be bound to the application.
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+
+    // Get the port assigned by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+
+    // return the app address to the caller
+    format!("http://127.0.0.1:{}", port)
 }
