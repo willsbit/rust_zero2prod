45fdaf3a534f6ffa779a746398b1ef4efa21e07c
Willian <willsbit@gmail.com>
2022-07-26 16:39:29 -0300

    [FIX] Compiler errors in run()

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ use zero2prod::run;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-
+    println!("{:?}", &listener);
     run(listener)?.await
 }
