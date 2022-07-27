607c840f0cf73d4f97ca3bf77727bd2ae1fbbd12
Willian <willsbit@gmail.com>
2022-07-27 13:28:57 -0300

    [FEATURE] Read config in main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::net::TcpListener;
 use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let configuraton = get_configuration().expect("Failed to read configuration.");
+
+    let address = format!("127.0.0.1:{}", configuraton.application_port);
+    let listener = TcpListener::bind(address)?;
     println!("{:?}", &listener);
     run(listener)?.await
 }
