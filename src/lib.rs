b703888d1748e37bd6bd6dcd13f8fef09bca095f
Willian <willsbit@gmail.com>
2022-07-27 11:42:16 -0300

    [INTERNAL] Restructure file tree

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,7 @@
-use actix_web::dev::Server;
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-use std::net::TcpListener;
+pub mod configuration;
+pub mod routes;
+pub mod startup;
 
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String
-}
+pub use routes::health_check;
+pub use routes::subscriptions;
 
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-    })
-    .listen(listener)?
-    .run();
-
-    Ok(server)
-}
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -25,7 +25,7 @@ fn spawn_app() -> String {
 
     // Get the port assigned by the OS
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let server = zero2prod::startup::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
 
     // return the app address to the caller
