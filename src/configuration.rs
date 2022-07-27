ec709870f208f4e31f22731d3316168f16f56f99
Willian <willsbit@gmail.com>
2022-07-27 14:34:07 -0300

    [FEATURE] Add function to return the postgres connection string

--- src/configuration.rs
+++ src/configuration.rs
@@ -15,6 +15,15 @@ pub struct DatabaseSettings {
     pub database_name: String,
 }
 
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
+}
+
 pub fn get_configuration() -> Result<Settings, ConfigError> {
     // initialize config reader
     let settings = Config::builder()
