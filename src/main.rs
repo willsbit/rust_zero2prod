77c0a3be64baed00b45fa9d6e0bb3e8a6666623a
Willian <willsbit@gmail.com>
2022-07-27 15:54:07 -0300

    [FEAT] Add interactions with the database for the subscriptions endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
-use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let configuraton = get_configuration().expect("Failed to read configuration.");
+    let configuration = get_configuration().expect("Failed to read configuration.");
 
-    let address = format!("127.0.0.1:{}", configuraton.application_port);
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
     println!("{:?}", &listener);
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
